67cec738a492bd0fd2912586badd2e83e8179c84
Gvilor <gvilor@yandex.ru>
2025-02-02 17:16:39 +0800

    Сбор монет

--- 3D Platformer/Assets/Player.cs
+++ 3D Platformer/Assets/Player.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Перменная для хранения монет
+    private int coins;
     //Переменная для огненного шара
     public GameObject fireballPrefab;
     // Точка вылета шара 
     public Transform attackPoint;
-
     //Максимальное здоровье игрока
     private int health = 10;
+
+    public void CollectCoins()
+    {
+        coins++; //Добавляем 1 монетку
+        print("Собранные монетки: " +  coins);//Выводим значение монеток
+    }
 
     //Метод понижающий здоровье игрока
     public void TakeDamage(int damage)

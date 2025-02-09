b382d5d1ccde85545a5c10fdf2bb1e208e50f4b7
Gvilor <gvilor@yandex.ru>
2025-02-09 20:33:40 +0800

    Исправил все ошибки
    Черепаха врезается в препятствия. Шар летит в центр экрана и уничтожает черепаху

---
+++ 3D Platformer/Assets/Code/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    //Перменная для хранения монет
+    private int coins;
+    //Переменная для огненного шара
+    public GameObject fireballPrefab;
+    //Точка вылета шара 
+    public Transform attackPoint;
+    //Максимальное здоровье игрока
+    private int health = 10;
+    //проигрывание звука
+    public AudioSource audioSource;
+    //файл содержащий звук урона
+    public AudioClip damageSound;
+
+    public void CollectCoins()
+    {
+        coins++; //Добавляем 1 монетку
+        print("Собранные монетки: " +  coins);//Выводим значение монеток
+    }
+
+    //Метод понижающий здоровье игрока
+    public void TakeDamage(int damage)
+    {
+        health -= damage; // уменьшаем здоровье
+
+        if (health > 0)
+        {
+            print("Здоровье игрока = " + health); //выводим кол-во жизней
+            audioSource.PlayOneShot(damageSound);//воспроизведение звука урона
+        }
+        else
+        {
+
+            //Получение индекса текущей сцены и ее перезапуск
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+    void Update()
+    {
+        //Если игрок нажимает левую кнопку мыши, создается огненный шар
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+    }
+}
